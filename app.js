2111d0f2a0e8fda4fb8be9dd96e23469738ce68d
InezMallandain <inezvmallandain@gmail.com>
2022-01-18 09:43:08 +0200

    Feat: add hamburger

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const container = document.querySelector('.container');
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
 
-open.addEventListener('click', () => container.classList.add('show-nav'));
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+let isMenuOpen = false;
+
+open.addEventListener("click", () => {
+  isMenuOpen = !isMenuOpen;
+  if (isMenuOpen) {
+    container.classList.add("show-nav");
+  } else {
+    container.classList.remove("show-nav");
+  }
+});
+
+var forEach = function (t, o, r) {
+  if ("[object Object]" === Object.prototype.toString.call(t))
+    for (var c in t)
+      Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+  else for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t);
+};
+
+var hamburgers = document.querySelectorAll(".hamburger");
+if (hamburgers.length > 0) {
+  forEach(hamburgers, function (hamburger) {
+    hamburger.addEventListener(
+      "click",
+      function () {
+        this.classList.toggle("is-active");
+      },
+      false
+    );
+  });
+}
